56a3fb0052c4a03a980efdd87359018df1c75ab2
samariums <svtslvvlsv@gmail.com>
2023-02-24 19:34:36 +0200

    повысил скорость ответа, изменил гит игнор

--- cache.py
+++ cache.py
@@ -2,6 +2,7 @@ import os
 import pickle
 from functools import wraps
 
+
 def cache(max_size=250):
     cache = {}
     cache_path = 'cache.pickle'
@@ -16,12 +17,14 @@ def cache(max_size=250):
             if str(args) in cache:
                 return cache[str(args)]
             result = func(*args)
-            if len(pickle.dumps(cache)) > max_size:
+            if len(pickle.dumps(cache, protocol=pickle.HIGHEST_PROTOCOL)) > max_size:
                 oldest = sorted(cache.keys())[0]
                 del cache[oldest]
-            cache[str (args)] = result
+            cache[str(args)] = result
             with open(cache_path, 'wb') as f:
                 pickle.dump(cache, f)
             return result
+
         return wrapper
+
     return decorator
