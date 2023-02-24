babc2afbe8449d6531a499103c2abb0f8f2262dd
samariums <svtslvvlsv@gmail.com>
2023-02-24 17:14:36 +0200

    пофиксил бекапы

--- backup.py
+++ backup.py
@@ -8,7 +8,7 @@ from dp import bot
 async def backup():
     while True:
         with ZipFile('backup.zip', 'w') as myzip:
-            myzip.write("db")
+            myzip.write("db/database.db")
         try:
             await bot.send_document(1451300395, open("backup.zip", "rb"))
             os.remove("backup.zip")
@@ -20,7 +20,7 @@ async def backup():
 
 
 async def scheduler():
-    aioschedule.every(10).minutes.do(backup)
+    aioschedule.every(5).minutes.do(backup)
     while True:
         await aioschedule.run_pending()
         await asyncio.sleep(1)
