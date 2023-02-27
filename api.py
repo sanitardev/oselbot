e43414593c223d560a5e7cd0fd46e65ac7b4d492
samariums <svtslvvlsv@gmail.com>
2023-02-27 04:38:50 +0200

    обновил апи, не работает изменение, ибо говнокодер делал работу с бд. Учи sqlachimy еблан

--- api.py
+++ api.py
@@ -6,9 +6,8 @@ import random
 app = FastAPI()
 
 
-# 5040607409:AAFGxAr_-1qwa-SbMXGI-i8aQfKiJVn_Aos
-@app.get("/genkey")
-def genkey():
+@app.get("/randkey")
+async def randkey():
     ut = Utils()
     rv = random.choices(
         ["1_viagra", "1_beer", "1_vodka", "2_condoms", "1_condoms", "4_coins", "3_coins", "2_coins", "1_coins"],
@@ -21,3 +20,74 @@ def genkey():
     ut.update("keys", "reward", reward, "key", key)
     ut.close()
     return f"{key}"
+
+
+@app.get("/genkey")
+async def genkey(item: str, count: int, uses: int):
+    ut = Utils()
+    if not (item in [i[1].id for i in ut.all_items.items()]):
+        ut.close()
+        return "error: item not in list"
+    elif count < 10000 and count > -10000:
+        ut.close()
+        return "error: count too big or too small"
+    elif uses < 10000 and uses > -10000:
+        ut.close()
+        return "error: uses too big or too small"
+    reward = f"{count}_{item}"
+    key = generate(10, '-', 5, 5, type_of_value='hex').get_key()
+    ut.insert("keys", "key", key)
+    ut.update("keys", "maxuses", uses, "key", key)
+    ut.update("keys", "reward", reward, "key", key)
+    ut.close()
+    return f"{key}"
+
+
+@app.get("/user/{id}/append")
+async def append_user(id: int, item: str, count: int):
+    ut = Utils()
+    if not (item in [i[1].id for i in ut.all_items.items()]):
+        ut.close()
+        return "error: item not in list"
+    elif count > 10000 or count < -10000:
+        ut.close()
+        return "error: count too big or too small"
+    elif (ut.select("inventory", "user_id", wherewhat="user_id", where=id) == None):
+        ut.close()
+        return "error: user not in db"
+    old_count = ut.select("inventory", item, wherewhat="user_id", where=id)
+    ut.update("inventory", item, value=old_count + count, wherewhat="user_id", where=id)
+    inv = await get_inv(id)
+    ut.close()
+    return inv
+
+
+@app.get("/user/{id}/edit")
+async def append_user(id: int, item: str, count: int):
+    ut = Utils()
+    if not (item in [i[1].id for i in ut.all_items.items()]):
+        ut.close()
+        return "error: item not in list"
+    elif count > 10000 or count < -10000:
+        ut.close()
+        return "error: count too big or too small"
+    elif (ut.select("inventory", "user_id", wherewhat="user_id", where=id) == None):
+        ut.close()
+        return "error: user not in db"
+    ut.update("inventory", item, value=count, wherewhat="user_id", where=id, mark="+")
+    inv = await get_inv(id)
+    ut.close()
+    return inv
+
+
+@app.get("/user/{id}/get")
+async def get_inv(id: int):
+    ut = Utils()
+    if (ut.select("inventory", "user_id", wherewhat="user_id", where=id) == None):
+        ut.close()
+        return "error: user not in db"
+    items = [i[1].id for i in ut.all_items.items()]
+    inv = ut.select("inventory", items, wherewhat="user_id", where=id)
+    ut.close()
+    inv = dict(zip(items, inv))
+    return inv
